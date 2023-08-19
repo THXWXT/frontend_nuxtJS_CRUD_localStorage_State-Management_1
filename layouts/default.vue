28b3df0e92781675d1de14373234307222aa810b
flook <tanawatflook@hotmail.com>
2023-08-19 14:30:47 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Nav />
+        <Nuxt />
+    </div>
+</template>
+
+<script >
+import { Nav } from "../components/Nav.vue";
+
+export default{
+    name: 'default',
+    comments:{
+        Nav
+    }
+}
+
+</script>
+
+<style  scoped></style>
