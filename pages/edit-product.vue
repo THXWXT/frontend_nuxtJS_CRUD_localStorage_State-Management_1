dd5c408787446ebfec16fa0a6a276262da1fa717
flook <tanawatflook@hotmail.com>
2023-08-19 22:12:24 +0700

    edit required

--- pages/edit-product.vue
+++ pages/edit-product.vue
@@ -5,27 +5,27 @@
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Name Product</span>
                 <input type="text" v-model="product.prod_name" class="form-control" aria-label="Username"
-                    aria-describedby="basic-addon1">
+                    aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Price Product</span>
                 <input type="text" v-model="product.prod_price" class="form-control" aria-label="Username"
-                    aria-describedby="basic-addon1">
+                    aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Product Desc</span>
                 <input type="text" v-model="product.prod_desc" class="form-control" aria-label="Username"
-                    aria-describedby="basic-addon1">
+                    aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">ID</span>
                 <input type="text" v-model="product.prod_id" class="form-control" aria-label="Username"
-                    aria-describedby="basic-addon1">
+                    aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
                 <span class="input-group-text" id="basic-addon1">Time</span>
                 <input type="date" v-model="product.prod_date" class="form-control" aria-label="Username"
-                    aria-describedby="basic-addon1">
+                    aria-describedby="basic-addon1" required>
             </div>
             <div class="form-group">
                 <button class="btn btn-primary btn-block" type="submit">Edit Book</button>
--- pages/index.vue
+++ pages/index.vue
@@ -7,27 +7,27 @@
             <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">Name Product</span>
               <input type="text" v-model="product.prod_name" class="form-control" aria-label="Username"
-                aria-describedby="basic-addon1">
+                aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">Price Product</span>
               <input type="text" v-model="product.prod_price" class="form-control" aria-label="Username"
-                aria-describedby="basic-addon1">
+                aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">Product Desc</span>
               <input type="text" v-model="product.prod_desc" class="form-control" aria-label="Username"
-                aria-describedby="basic-addon1">
+                aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">ID</span>
               <input type="text" v-model="product.prod_id" class="form-control" aria-label="Username"
-                aria-describedby="basic-addon1">
+                aria-describedby="basic-addon1" required>
             </div>
             <div class="input-group mb-3">
               <span class="input-group-text" id="basic-addon1">Time</span>
               <input type="date" v-model="product.prod_date" class="form-control" aria-label="Username"
-                aria-describedby="basic-addon1">
+                aria-describedby="basic-addon1" required>
             </div>
             <div class="form-group">
               <button class="btn btn-primary btn-block" type="submit">Add Book</button>
@@ -57,19 +57,10 @@
     },
     methods: {
         saveProduct(){
-          
         const products = JSON.parse(localStorage.getItem('products')) || [];
           this.$store.dispatch("addItem",this.product).then(()=>{
             this.$router.push("/list-product");
-          })
-        this.product = {
-          prod_name: '',
-          prod_price: '',
-          prod_desc: '',
-          prod_id: '',
-          prod_date: ''
-        };
-          
+          })          
         }
       },
   }
